029cce56e2a147a410d9dab4f3b16f377dca1fc3
Luca Austelat <Luca.Austelat@gmx.de>
2020-03-21 21:02:54 +0100

    default return json returns array if data empty

--- app/helper/helper.py
+++ app/helper/helper.py
@@ -4,4 +4,5 @@ from flask import jsonify
 
 
 def return_json(data: Union[list, dict] = None, status: int = 200, message: str = ""):
+    data = data if data else []
     return jsonify({'status': status, 'message': message, 'results': data})
