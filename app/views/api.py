de0e024c82690038d319cd114a76e8fb6c09c549
Luca Austelat <Luca.Austelat@gmx.de>
2020-03-22 00:28:38 +0100

    remove redundant test code

--- app/views/api.py
+++ app/views/api.py
@@ -13,22 +13,8 @@ log.add(f"{os.path.join(DefaultPaths.LOG_PATH)}/api.log", rotation="5 MB",
 
 @app.route('/', methods=['GET'])
 def show_hello():
-
-    log.debug("show_hello()")
     data = {
         'status': 200,
         'message': "Hello"
     }
     return jsonify(data)
-
-
-@app.route('/info', methods=['GET'])
-@token_required
-def show_api_info():
-
-    info = {
-        'version': "0.1"
-    }
-
-    log.debug("Test log")
-    return jsonify(info)
