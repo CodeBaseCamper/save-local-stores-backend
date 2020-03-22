ae5ea9c108794f065e6033cbe643b3df4a030152
Luca Austelat <Luca.Austelat@gmx.de>
2020-03-22 01:17:09 +0100

    error handler return now json

--- app/views/errorhandlers.py
+++ app/views/errorhandlers.py
@@ -1,18 +1,23 @@
 # coding=utf-8
-from app import app
+from app import app, return_json
 from flask import render_template, abort
 
 
 @app.errorhandler(403)
 def forbidden(e):
-    abort(403)
+    return return_json(status=403, message="Forbidden")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    abort(404)
+    return return_json(status=404, message="Page not found!")
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    abort(500)
+    return return_json(status=500, message="Internal Server Error!")
+
+
+@app.errorhandler(502)
+def bad_gateway(e):
+    return return_json(status=502, message="Bad Gateway!")
